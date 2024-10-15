214732a502213efaaafcb9ca2a6826130d782c5a
snan123344 <snan@mohanishx.link>
2024-10-15 09:56:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -676,7 +676,7 @@ app.use((err, req, res, next) => {
 
 // === Start the Server ===
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
